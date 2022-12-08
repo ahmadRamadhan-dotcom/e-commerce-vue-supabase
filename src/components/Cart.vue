07eed1535885773f8311a963db5a0b85862ae99e
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-12-08 14:11:54 +0700

    chore: responsive header

---
+++ src/components/Cart.vue
@@ -0,0 +1,13 @@
+<template>
+  <button id="cart" class="relative">
+    <img :src="cart" alt="" />
+    <span
+      class="bg-red-500 hidden absolute -top-3 -right-3 px-2 font-mediumtext-center text-white rounded-full text-[12px]"
+      >1</span
+    >
+  </button>
+</template>
+
+<script setup>
+import cart from "~/assets/cart.svg";
+</script>
---
+++ src/components/Whislist.vue
@@ -0,0 +1,13 @@
+<script setup>
+import whislist from "~/assets/whislist.svg";
+</script>
+
+<template>
+  <button class="relative">
+    <img :src="whislist" alt="" />
+    <span
+      class="bg-red-500 hidden absolute -top-3 -right-3 px-2 font-mediumtext-center text-white rounded-full text-[12px]"
+      >1</span
+    >
+  </button>
+</template>
