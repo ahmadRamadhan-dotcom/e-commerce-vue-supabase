3ccc4789808b0892b1d62ccec259debace2886b2
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-12-08 15:20:27 +0700

    chore: responsive hero

---
+++ src/components/home/Hero.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gray-100 p-10 mt-8 h-[7em]">
+    <h1 class="font-semibold text-2xl lg:text-3xl">All Products</h1>
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,9 +3,12 @@
     class="mt-8 mx-auto w-11/12 sm:w-9/12 md:w-11/12 xl:w-[1130px] 2xl:w-[1300px]"
   >
     <Breadcumb />
+    <Hero />
   </main>
 </template>
 
 <script setup>
+// internal imports
 import Breadcumb from "~/components/home/Breadcumb.vue";
+import Hero from "~/components/home/Hero.vue";
 </script>
