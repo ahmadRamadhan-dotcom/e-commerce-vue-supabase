b74cb704bed360f4d63e476a28a0194099d60e20
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-12-08 21:33:46 +0700

    chore: responsive main content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <main
-    class="mt-8 mx-auto w-11/12 sm:w-9/12 md:w-11/12 xl:w-[1130px] 2x:w-[1300px]"
+    class="mt-8 mx-auto w-11/12 sm:w-9/12 md:w-11/12 xl:w-[1130px] 2xl:w-[1300px]"
   >
     <Breadcumb />
     <Hero />
-    <div class="grid mt-4 xl:grid-cols-5">
+    <p class="mt-3 text-black/50">
+      <span class="text-black font-medium">{{ products.length }}</span> Product
+      found
+    </p>
+    <div
+      class="mt-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-0 xl:grid-cols-5"
+    >
       <ProductCard :products="products" />
     </div>
   </main>
